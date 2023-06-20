040c1d983c9641a88fc61912bd7d697c4624140e
Aljhun Legaspi <aljhunlegaspi890@gmail.com>
2023-06-20 10:11:04 +0800

    removed unused code in initialize function

--- listing_plugin.php
+++ listing_plugin.php
@@ -27,10 +27,6 @@ if(!class_exists('ListingPlugin')){
 
         public function initialize()
         {
-            //   include_once MY_PLUGIN_PATH . 'includes/utilities.php';
-
-            //   include_once MY_PLUGIN_PATH . 'includes/options-page.php';
-
               include_once MY_PLUGIN_PATH . 'includes/listings.php';
         }
     }
